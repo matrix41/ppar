98eaae92000b5710967b477e9aaaa35ee6961ee0
matrix41 <raymond@vivaldi.ipac.caltech.edu>
2014-05-19 11:27:12 -0700

    added all 95 planet parameters keys to the hash function

--- ppar.pl
+++ ppar.pl
@@ -2,14 +2,108 @@
 use strict;
 
 # Q. What exactly is a hash and a hash reference?  
-# A. 
+# A. *hands you an apple* pretend that's a hash. 
+#    *hands you written directions to an apple in the fridge* 
+#    that's a hash reference.
 
 
 # Step 1 of 3: Initialize the hash array 
 my %hash = (
 	plnorbper => 'null',
 	plnorbpererr1 => 'null',
-	plnorbpererr2 => 'null'
+	plnorbpererr2 => 'null',
+	plnorbperlim => 'null',
+	plnorbsmax => 'null',
+	plnorbsmaxerr1 => 'null',
+	plnorbsmaxerr2 => 'null',
+	plnorbsmaxlim => 'null',
+	plnorbincl => 'null',
+	plnorbinclerr1 => 'null',
+	plnorbinclerr2 => 'null',
+	plnorbincllim => 'null',
+	plnorbtper => 'null',
+	plnorbtpererr1 => 'null',
+	plnorbtpererr2 => 'null',
+	plnorbtperlim => 'null',
+	plnorbeccen => 'null',
+	plnorbeccenerr1 => 'null',
+	plnorbeccenerr2 => 'null',
+	plnorbeccenlim => 'null',
+	plnorblper => 'null',
+	plnorblpererr1 => 'null',
+	plnorblpererr2 => 'null',
+	plnorblperlim => 'null',
+	plnorbdate => 'null',
+	plnorbmethod => 'null',
+	plnrvamp => 'null',
+	plnrvamperr1 => 'null',
+	plnrvamperr2 => 'null',
+	plnrvamplim => 'null',
+	plnmsinij => 'null',
+	plnmsinijerr1 => 'null',
+	plnmsinijerr2 => 'null',
+	plnmsinie => 'null',
+	plnmsinieerr1 => 'null',
+	plnmsinieerr2 => 'null',
+	plnmsinilim => 'null',
+	plnmassj => 'null',
+	plnmassjerr1 => 'null',
+	plnmassjerr2 => 'null',
+	plnmasse => 'null',
+	plnmasseerr1 => 'null',
+	plnmasseerr2 => 'null',
+	plnmasslim => 'null',
+	plnradj => 'null',
+	plnradjerr1 => 'null',
+	plnradjerr2 => 'null',
+	plnrade => 'null',
+	plnradeerr1 => 'null',
+	plnradeerr2 => 'null',
+	plnrads => 'null',
+	plnradserr1 => 'null',
+	plnradserr2 => 'null',
+	plnradlim => 'null',
+	plndens => 'null',
+	plndenserr1 => 'null',
+	plndenserr2 => 'null',
+	plndenslim => 'null',
+	plneqt => 'null',
+	plneqterr1 => 'null',
+	plneqterr2 => 'null',
+	plneqtlim => 'null',
+	plntrandep => 'null',
+	plntrandeperr1 => 'null',
+	plntrandeperr2 => 'null',
+	plntrandeplim => 'null',
+	plntrandurd => 'null',
+	plntrandurderr1 => 'null',
+	plntrandurderr2 => 'null',
+	plntrandurh => 'null',
+	plntrandurherr1 => 'null',
+	plntrandurherr2 => 'null',
+	plntrandurlim => 'null',
+	plntranmid => 'null',
+	plntranmiderr1 => 'null',
+	plntranmiderr2 => 'null',
+	plntranmidlim => 'null',
+	plnimppar => 'null',
+	plnimpparerr1 => 'null',
+	plnimpparerr2 => 'null',
+	plnimpparlim => 'null',
+	plnoccdep => 'null',
+	plnoccdeperr1 => 'null',
+	plnoccdeperr2 => 'null',
+	plnoccdeplim => 'null',
+	plnratdor => 'null',
+	plnratdorerr1 => 'null',
+	plnratdorerr2 => 'null',
+	plnratdorlim => 'null',
+	plnratror => 'null',
+	plnratrorerr1 => 'null',
+	plnratrorerr2 => 'null',
+	plnratrorlim => 'null',
+	plnblend => 'null',
+	plnrefid => 'null'
 );
 
 
@@ -17,7 +111,7 @@ my %hash = (
 # (do this in a FOR-loop; hit Ctrl-C to break out of loop)
 
 #damn. how do i do a FOR-loop in perl????
-print 'Enter key and value pair (key <space> value; press Ctrl-C to exit) ==>';
+print 'Enter key and value pair (key <space> value; press Ctrl-C to exit) =>';
 print "\n";
 
 
